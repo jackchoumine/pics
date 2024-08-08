038ec9dcbff84cbc3165094dcfa2b9916aab9de2
ZhouQiJun <zhouqijun4job1@163.com>
2024-08-08 15:44:16 +0800

    feat: 配置 husky 和 commitlint 检查提交信息

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2024-08-08 15:29:37
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2024-08-08 15:29:44
+ * @Description : git commit 规范配置
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+}
