674161b1260a041dbcc461665fbc2cc702f5d826
ZhouQiJun <zhouqijun4job1@163.com>
2024-08-08 17:38:39 +0800

    feat: 添加emoji

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,9 +2,32 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-08-08 15:29:37
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-08-08 15:29:44
+ * @LastEditTime: 2024-08-08 17:35:26
  * @Description : git commit 规范配置
  */
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['gitmoji'],
+  // rules: {
+  //   // type 类型定义
+  //   'type-enum': [
+  //     2, // 2 表示必须
+  //     'always',
+  //     [
+  //       '🐛fix',
+  //       '♻️refactor',
+  //       '⚡️perf',
+  //       '🔀merge',
+  //       '✅test',
+  //       '🎨format',
+  //       '📝docs',
+  //       '⏪️revert',
+  //       '🔨build',
+  //       '👷ci',
+  //       '🔧chore',
+  //       '🔖release',
+  //     ],
+  //   ],
+  //   // subject 大小写不做校验
+  //   'subject-case': [0],
+  // },
 }
