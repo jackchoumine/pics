9c10b83b6db28ad4b4fce91f9772fc7dd575f9b8
ZhouQiJun <zhouqijun4job1@163.com>
2024-10-28 11:11:17 +0800

    增加自动推送

--- push.js
+++ push.js
@@ -1,13 +1,38 @@
 const { exec } = require('child_process')
-
-exec('git push', (err, stdout, stderr) => {
-  console.log({
-    err,
-    stdout,
-    stderr,
+const interval = 5000
+repeat(stop => {
+  exec('git push', (err, stdout, stderr) => {
+    // console.log({
+    //   err,
+    //   stdout,
+    //   stderr,
+    // })
+    if (!err) {
+      console.log('提交已推送')
+      stop()
+    } else {
+      console.log('推送失败')
+      console.log(err.message)
+      console.log('继续尝试推送')
+    }
+    // handle err, stdout & stderr
   })
-  if (!err) {
-    console.log('提交已推送')
-  }
-  // handle err, stdout & stderr
 })
+
+function repeat(fn) {
+  let hasStopped = false
+  let timer2
+  let timer = setTimeout(function repeatMe() {
+    if (hasStopped) {
+      return
+    }
+    fn(stop)
+    timer2 = setTimeout(repeatMe, interval)
+  }, interval)
+  return stop
+  function stop() {
+    hasStopped = true
+    clearTimeout(timer)
+    clearTimeout(timer2)
+  }
+}
